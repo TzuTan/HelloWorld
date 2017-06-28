499165e05df8bf477883a678b7821051bbfd7f19
TzuTan <tzutan@nature.ee.ncku.edu.tw>
2017-06-28 22:45:00 +0800

    Change

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
+    	String greeting = Greetings();
         System.out.println("Hello, World");
     }
+    
+    public static String Greetings(){
+        return "Hi";
+    }
+        
 }
